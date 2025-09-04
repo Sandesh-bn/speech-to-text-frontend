5de8720fbe41c90ffc8913dadef098c0ddeb6b0c
Sandesh-bn <sandesh647bn@gmail.com>
2025-09-04 01:44:55 -0700

    Minor layout changes

--- src/components/Transcripts.js
+++ src/components/Transcripts.js
@@ -39,38 +39,41 @@ export default function Transcripts({ transcript, updateSpeechBubbles }) {
         <div style={{ maxWidth: "600px", fontFamily: "Arial" }}>
             {!isEditing ? (
                 <div>
+                    <button className="transcripts-button" onClick={handleEdit}>Edit Transcript</button>
                     <pre
-                        
                         style={{
                             border: "1px solid #ccc",
                             padding: "10px",
                             borderRadius: "6px",
-                            marginBottom: "10px",
+                            marginBottom: "20px",
                             backgroundColor: "#f9f9f9",
                             textWrap: 'wrap',
                             whiteSpace: 'pre-wrap',
-                            fontSize: '15px'
+                            fontSize: '15px',
+                            overflowY: 'scroll',
+                            maxHeight: '300px'
                         }}
                     >
                         {text}
                     </pre>
-                    <button className="transcripts-button" onClick={handleEdit}>Edit</button>
+                    
                 </div>
             ) : (
                 <div>
-                    <textarea
-                        rows="7"
-                        style={{ width: "600px", padding: "8px", marginBottom: "10px" }}
-                        
-                        value={draft}
-                        onChange={(e) => setDraft(e.target.value)}
-                    />
                     <div>
                         <button className="transcripts-button"  onClick={handleSubmit}>Save</button>
                         <button className="transcripts-button"  onClick={handleCancel} style={{ marginLeft: "8px" }}>
                             Cancel
                         </button>
                     </div>
+                    <textarea
+                        rows="12"
+                        style={{ width: "600px", padding: "8px", marginBottom: "10px" }}
+                        
+                        value={draft}
+                        onChange={(e) => setDraft(e.target.value)}
+                    />
+                    
                 </div>
             )}
         </div>
